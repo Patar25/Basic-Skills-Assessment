ae9074ae037eac5229049a68d0e766af7d71df26
Patrick ODonoghue <tullaandgimley@gmail.com>
2020-09-17 17:26:38 +0200

    final animation changes

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ $(document).ready(function () {
         nextArrow: '<span class="next-arrow"><i class="fas fa-arrow-right fa-2x"></i></span>'
     });
 });
+
+// Lazy loading
+
+(function () {
+    var elements;
+    var windowHeight;
+
+    function init() {
+        elements = document.querySelectorAll('.hidden');
+        windowHeight = window.innerHeight;
+    }
+
+    function checkPosition() {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var positionFromTop = elements[i].getBoundingClientRect().top;
+
+            if (positionFromTop - windowHeight <= 0) {
+                element.classList.add('fade-in-element');
+                element.classList.remove('hidden');
+            }
+        }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+
+    init();
+    checkPosition();
+})();
